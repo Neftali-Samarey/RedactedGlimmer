931c7dd631da6f3e481fc1a6968a2fbad306c208
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2022-09-27 19:37:23 -0400

    Updated sample view content

--- ShimmeringRedacted/ContentView.swift
+++ ShimmeringRedacted/ContentView.swift
@@ -12,28 +12,40 @@ struct ContentView: View {
     @State var isLoading: Bool = false
 
     var body: some View {
-        VStack(spacing: 5) {
-            Image("Portrait")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 200, height: 300)
-                .cornerRadius(3)
-                .redacted(reason: isLoading ? .placeholder : [])
-                .glimmering(active: isLoading)
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 5) {
+                    Image("Portrait")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 300)
+                        .cornerRadius(3)
+                        .redacted(reason: isLoading ? .placeholder : [])
+                        .glimmering(active: isLoading)
 
-            Text("by")
-                .italic()
-                .unredacted()
+                    Text("by")
+                        .italic()
+                        .unredacted()
 
-            Text("Alison Green")
-                .frame(width: 320, height: 24)
-                .font(.title2)
-                .redacted(reason: isLoading ? .placeholder : [])
-                .glimmering(active: isLoading)
+                    Text("Alison Green")
+                        .frame(width: 320, height: 24)
+                        .font(.title2)
+                        .redacted(reason: isLoading ? .placeholder : [])
+                        .glimmering(active: isLoading)
+
+                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                        .font(.body)
+                        .redacted(reason: isLoading ? .placeholder : [])
+                        .glimmering(active: isLoading)
+                        .padding()
+                }
+                .onAppear(perform: {
+                    simulateNetworkCall()
+                })
+            }
+            .navigationTitle("Hiking Day")
+            .padding()
         }
-        .onAppear(perform: {
-            simulateNetworkCall()
-        })
     }
 
     func simulateNetworkCall() {
